25fa38a03fb8aa60a9573c573541b9de3cc0a2e2
AhmedWaleedKalifa <ahmed2210waleed@gmail.com>
2024-02-07 13:16:28 +0200

    feat: make a playRound function that makes player can play single round

--- js.js
+++ js.js
@@ -8,3 +8,23 @@ function getComputerChoice(){
         return "Paper";
     }
 }
+function playRound(computerSelection,playerSelection){
+    playerSelection=(playerSelection.at(0).toUpperCase()+playerSelection.slice(1).toLowerCase());
+    if(computerSelection==playerSelection){
+    return "Draw";
+    }else if(computerSelection=="Rock"&&playerSelection=="Paper"){
+    return "You Won! Paper beats Rock.";
+    }else if(computerSelection=="Rock"&&playerSelection=="Scissors"){
+    return "You Lose! Rock beats Scissors.";
+    }else if(computerSelection=="Paper"&&playerSelection=="Rock"){
+    return "You Lose! Paper beats Rock.";
+    }else if(computerSelection=="paper"&&playerSelection=="Scissors"){
+    return "You Won! Scissors beats Paper.";
+    }else if(computerSelection=="Scissors"&&playerSelection=="Rock"){
+    return "You Won! Rock beats Scissors.";
+    }else if(computerSelection=="Scissors"&&playerSelection=="Paper"){
+    return "You Lose! Scissors beats Paper.";
+    }else{
+    return "The input is wrong try again.";
+    }
+    }
