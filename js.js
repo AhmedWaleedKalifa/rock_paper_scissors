ba0ab19227486fc04289c1229f66ea5d73fbaab9
AhmedWaleedKalifa <ahmed2210waleed@gmail.com>
2024-02-07 13:12:38 +0200

    feat: makeComputerChoice function

---
+++ js.js
@@ -0,0 +1,10 @@
+function getComputerChoice(){
+    let number= Math.floor(Math.random()*3)+1;
+    if(number==1){
+        return "Rock";
+    }else if(number==2){
+        return "Scissors";
+    }else{
+        return "Paper";
+    }
+}
